1428db411b2c759fdb171b1160260149597cec75
Bruno Britto <bruno_britto10@hotmail.com>
2023-09-22 13:50:03 +0200

    Projection of authors data with DTO

--- src/main/java/com/brunobritto/Apirestfulmongodb/config/Instantiation.java
+++ src/main/java/com/brunobritto/Apirestfulmongodb/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.brunobritto.Apirestfulmongodb.domain.Post;
 import com.brunobritto.Apirestfulmongodb.domain.User;
+import com.brunobritto.Apirestfulmongodb.dto.AuthorDTO;
 import com.brunobritto.Apirestfulmongodb.repository.PostRepository;
 import com.brunobritto.Apirestfulmongodb.repository.UserRepository;
 
@@ -35,10 +36,11 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2023"), "Partiu viagem", "Vou viajar para SP abraços", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2023"), "Bom dia", "Acordei feliz hoje!", maria);
-	
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2023"), "Partiu viagem", "Vou viajar para SP abraços", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2023"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+	
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
