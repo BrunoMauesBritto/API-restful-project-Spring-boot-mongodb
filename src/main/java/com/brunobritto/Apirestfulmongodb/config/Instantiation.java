8df3d3afd90fce548c0f021544c8a53c078edf8f
Bruno Britto <bruno_britto10@hotmail.com>
2023-09-22 15:16:54 +0200

    Adding comments to posts

--- src/main/java/com/brunobritto/Apirestfulmongodb/config/Instantiation.java
+++ src/main/java/com/brunobritto/Apirestfulmongodb/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.brunobritto.Apirestfulmongodb.domain.Post;
 import com.brunobritto.Apirestfulmongodb.domain.User;
 import com.brunobritto.Apirestfulmongodb.dto.AuthorDTO;
+import com.brunobritto.Apirestfulmongodb.dto.CommentDTO;
 import com.brunobritto.Apirestfulmongodb.repository.PostRepository;
 import com.brunobritto.Apirestfulmongodb.repository.UserRepository;
 
@@ -41,6 +42,13 @@ public class Instantiation implements CommandLineRunner {
 		Post post1 = new Post(null, sdf.parse("21/03/2023"), "Partiu viagem", "Vou viajar para SP abraços", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2023"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 	
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2023"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
