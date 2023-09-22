fa7e102a421026e35d50e141e12821c514ebccac
Bruno Britto <bruno_britto10@hotmail.com>
2023-09-22 09:59:53 +0200

    Deletion of user with DELETE

--- src/main/java/com/brunobritto/Apirestfulmongodb/services/UserService.java
+++ src/main/java/com/brunobritto/Apirestfulmongodb/services/UserService.java
@@ -22,13 +22,20 @@ public class UserService {
 	}
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado"));
 	}
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
 	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+	
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(),objDto.getEmail());
 	}
+	
+	
 }
